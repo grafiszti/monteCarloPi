e9634f68f17bca39b1d87c09a2cfec11fa14534f
Matt <grafiszti@gmail.com>
2015-09-07 23:56:33 +0200

    added runing time

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
+import monteCarlo.Calculable;
+import monteCarlo.bfCode.Calculator;
 import monteCarlo.s.C;
 
+import java.util.Date;
+
 public class Main {
-    public static final int N = 100000;
+    public static final int N = 1000000;
+
     public static void main(String args[]) {
-        printResult(new monteCarlo.bfCode.Calculator().calculate(N));
-        printResult(new C().c(N));
+        execute(new Calculator(), N);
+
+        Date startDate = new Date();
+        double pi = new C().c(N);
+        Date endDate = new Date();
+        printResult(pi, endDate.getTime() - startDate.getTime());
     }
 
-    private static void printResult(double pi) {
-        System.out.println("Pi ~ " + pi);
+    private static void execute(Calculable calculator, int n) {
+        Date startDate = new Date();
+        double pi = calculator.calculate(n);
+        Date endDate = new Date();
+        printResult(pi, endDate.getTime() - startDate.getTime());
+    }
+
+    private static void printResult(double pi, long milis) {
+        System.out.println("Pi ~ " + pi + " in: " + (double) milis / 1000 + " seconds.");
     }
 }
